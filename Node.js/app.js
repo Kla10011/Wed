46b84985f736c13057dbd404b7474d2b28eca013
Kla10011 <klaza565@gmail.com>
2024-07-03 13:00:00 +0700

    add

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,13 +1,20 @@
-const express = require('express');
+import express from 'express'
+import chalk from 'chalk';
+import debug from 'debug';
+import morgan from 'morgan';
+// const express = require('express');
+// const chalk = require('chalk');
 const app = express();
 const port = 3000;
-const chalk = require('chalk');
+
+app.use(morgan("combined"));
 
 app.get("/",(req,res) =>{
     res.send('hello ronnakon mekvimanloi');
 })
 
 app.listen(port,()=>{
-    console.log("listening on port %d",port);
-    // console.log("listening on port %d"+chalk.green(port));
+    // console.log("listening on port %d",port);
+    console.log("listening on port %d"+chalk.green(port));
+    debug("listening on port %d"+chalk.green(port));
 })
